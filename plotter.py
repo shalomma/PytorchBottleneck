38dd7024bcdd31403598297619e9e1ab139ae3ba
Maayan Shalom <sha.maayan@gmail.com>
2020-03-24 21:57:46 +0200

    fix plots

--- plotter.py
+++ plotter.py
@@ -6,6 +6,7 @@ import matplotlib.ticker as ticker
 class Plotter:
     def __init__(self, trainer):
         self.trainer = trainer
+        self.n_layers = self.trainer.config.model.n_layers
 
     def plot_losses(self):
         plt.figure()
@@ -23,25 +24,32 @@ class Plotter:
         plt.savefig('acc.png')
         plt.show()
 
+    def format_epochs(self, x, pos):
+        return int(x * self.trainer.mi_cycle)
+
     def plot_info_plan(self, phase):
-        plt.figure()
+        running_mis_xt = np.array(self.trainer.running_mis_xt[phase])
+        running_mis_ty = np.array(self.trainer.running_mis_ty[phase])
+
+        fig, ax = plt.subplots()
         plt.title(phase)
-        plt.plot(self.trainer.running_mis_xt[phase])
+        for i in range(self.n_layers):
+            plt.plot(running_mis_xt[:, i], label=f'{i}')
+        plt.legend()
+        ax.xaxis.set_major_formatter(ticker.FuncFormatter(self.format_epochs))
         plt.ylabel('I(X;T)')
         plt.savefig(f'plot_{phase}.png')
         plt.show()
 
-        running_mis_xt = np.array(self.trainer.running_mis_xt[phase])
-        running_mis_ty = np.array(self.trainer.running_mis_ty[phase])
         plt.figure()
         plt.title(f'IP - {phase}')
         c = list(range(len(running_mis_xt[:, 0])))
-        for j in range(len(self.trainer.config.model.hidden_sizes)):
+        for j in range(self.n_layers):
             plt.scatter(running_mis_xt[:, j], running_mis_ty[:, j], c=c, cmap='plasma', s=20, alpha=0.85, zorder=1)
         for j in range(len(running_mis_xt[:, 0])):
             plt.plot(running_mis_xt[j, :], running_mis_ty[j, :], alpha=0.1, zorder=0)
 
-        cbar = plt.colorbar(format=ticker.FuncFormatter(lambda x, pos: int(x * self.trainer.mi_cycle)))
+        cbar = plt.colorbar(format=ticker.FuncFormatter(self.format_epochs))
         cbar.set_label('Epochs')
 
         plt.xlabel('I(X;T)')
